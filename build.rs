5bde2b944d4deb14cf87d6e1d98ea1c3c69eb49a
Naja Melan <najamelan@autistici.org>
2025-02-04 20:01:59 +0100

    Fix warning for nightly cfg

--- build.rs
+++ build.rs
@@ -4,6 +4,14 @@ use rustc_version::{ version_meta, Channel };
 
 fn main()
 {
+	// Needed to avoid warnings for:
+	// https://doc.rust-lang.org/nightly/rustc/check-cfg/cargo-specifics.html
+	//
+	println!("cargo::rustc-check-cfg=cfg(stable)");
+	println!("cargo::rustc-check-cfg=cfg(beta)");
+	println!("cargo::rustc-check-cfg=cfg(nightly)");
+	println!("cargo::rustc-check-cfg=cfg(rustc_dev)");
+
 	// Set cfg flags depending on release channel
 	//
 	match version_meta().unwrap().channel
